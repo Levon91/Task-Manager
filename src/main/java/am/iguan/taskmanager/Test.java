2c76cc711e5eeb3fff7ba47156844a65bf1bbdcf
Dmitriy Mihalev <dima.orlov1111@gmail.com>
2014-12-13 12:43:52 +0400

    created project

---
+++ src/main/java/am/iguan/taskmanager/Test.java
@@ -0,0 +1,10 @@
+package am.iguan.taskmanager;
+
+/**
+ * Created by Admin on 13.12.2014.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
